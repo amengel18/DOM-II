9d8b1847f8ef28de15e7244609b2ec3cc1de5249
Angela Engel <amengel.ae@icloud.com>
2021-01-17 16:44:51 -0700

    4 events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+
+const navBar = document.querySelector(".nav-link");
+navBar.addEventListener('mouseover', event => {
+    event.target.style.color = 'purple'
+});
+navBar.addEventListener('mouseout', event => {
+    event.target.style.color = 'pink'
+});
+
+
+let header = document.querySelector('h1')
+header.addEventListener ('mouseenter', event => {
+    event.target.style.color = 'red'
+});
+header.addEventListener('mouseleave', event => {
+    event.target.style.color = 'green'
+});
+
+
+
+
+
